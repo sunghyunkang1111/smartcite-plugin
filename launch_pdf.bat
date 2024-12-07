7e285b53b568616cc9d0de9600e402995ce674bc
unknown <madmoloh@icloud.com>
2024-12-07 08:09:58 +0700

    cleaned folder, added basic plugin

--- launch_pdf.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python "C:\Program Files\Adobe\Acrobat DC\Acrobat\Javascripts\download_and_open.py"
-pause
--- test.bat
+++
@@ -1,6 +0,0 @@
-
-echo %BLA% > C:\Program Files\Adobe\Acrobat DC\Acrobat\Javascripts\test.txt
-
-$.setenv("BLA","value");
-
-File("C:\\Program Files\\Adobe\\Acrobat DC\\Acrobat\\Javascripts\\test.bat").execute();
